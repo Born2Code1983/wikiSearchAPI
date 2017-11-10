c5f3f4798be3435c3ca2307c7ca28becc19540d2
Rigo De La Torre <rigoddd@gmail.com>
2017-11-10 00:02:24 -0800

    wiki Search API

---
+++ js/wikiPage.js
@@ -0,0 +1,47 @@
+
+$(document).ready(function() {
+  
+      $('#searchButton').click(function() {
+          getWiki();
+      });
+  
+      $("#mySearch").keypress(function(e) {
+          if (e.keyCode === 13) {
+              getWiki();
+          }
+      });
+      
+      function getWiki() {
+        var wikiInfo = $('#showWikiInfo');
+        var searchTerm = $('#mySearch').val();
+        var urlPlz = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&format=json&callback=?";
+      
+       
+  
+          $.ajax( /*Gets the weather info from API    */ {
+              type: 'GET',
+              url: urlPlz,
+              dataType: 'json',
+
+              success: function(data) {
+                  console.log(data);
+                  wikiInfo.html('');
+                  for (var i = 0; i < data[1].length; i++) {
+                      wikiInfo.prepend("<li id ='showWikiInfo'><a href=" + data[3][i] + " target=" + "blank" + "><h3>" + data[1][i] + "</h3></a><p>" + data[2][i] + "</p></li>");
+                  }
+  
+              },
+              error: function(errorMessage) {
+                  alert("Error");
+              }
+  
+  
+  
+  
+          });
+      };
+  });
+
+
+
+
